b152a12e50afe12d7d76f070a3d155077bb6e241
timbl <timbl>
2001-05-21 03:11:59 +0000

    after changes to namespace and use of this with forall

--- test/retest.sh
+++ test/retest.sh
@@ -14,17 +14,21 @@ function cwm_test () {
   shift; shift;
   args=$*
   echo
-  echo TEST $case: $desc
-  echo TEST cwm $args
-  # Hmm... this suggests a --nocomments flag on cwm
-  (python ../cwm.py $args | perl -ne 'print unless /^\s*#/ || /\$Id/ || /Id:/' > temp/$case) || echo CRASH $case
-  diff -bwu ref/$case temp/$case >diffs/$case
-  if [ -s diffs/$case ]; then echo FAIL: $case: see diffs/$case; else echo PASS $case; fi
+  echo Test $case: $desc
+
+  echo Test    cwm $args
+  # Hmm... this suggests a --nocomments flag on cwm  its -quiet
+#  (python ../cwm.py $args | sed -e 's/^ *#.*//' > temp/$case) || echo CRASH $case
+  (python ../cwm.py -quiet $args > temp/$case) || echo CRASH $case
+  diff -Bbwu ref/$case temp/$case >diffs/$case
+  if [ -s diffs/$case ]; then echo FAIL: $case: see diffs/$case; else echo Pass $case; fi
 }
 
 cwm_test animal.n3 "Parse a small RDF file, generate N3" -rdf animal.rdf -n3
 
 cwm_test animal-1.rdf "Parse a small RDF file" -rdf animal.rdf
+
+cwm_test daml-ont.n3 "Convert some RDF/XML into RDF/N3" daml-pref.n3 -rdf daml-ont.rdf -n3
 
 cwm_test strquot.n3 "N3 string quoting" -n3 strquot.n3
 
@@ -34,28 +38,32 @@ cwm_test lists-simple.n3 "parsing and generation of N3 list () syntax" -n3 lists
 
 cwm_test lists-simple-1.rdf "conversion of N3 list () syntax to RDF" -n3 lists-simple.n3 -rdf
 
-exit 0 # Tim, please update the expected results (ref/*)
-       # of the rest of these tests. -- DWC
+#exit 0 # Tim, please update the expected results (ref/*)
+#       # of the rest of these tests. -- DWC
+# Should work - tim
 
-cwm_test daml-ont.n3 "dan doesn't grok this one@@" daml-pref.n3 -rdf daml-ont.rdf -n3
-
+# The prefix file is to give cwm a hint for output. It saves hints across files.
+cwm_test daml-ont.n3 "Convert DAML schema into N3" daml-pref.n3 -rdf daml-ont.rdf -n3
 
 cwm_test daml-ex.n3 "Try the examples" daml-pref.n3 -rdf daml-ex.rdf -n3
 
-cwm_test rules12-1.n3 "Try some inference @@what is this test really about?" rules12.n3 -rules
+cwm_test rules12-1.n3 "log:implies Rules - try one iteration first." rules12.n3 -rules
 
-cwm_test rules12-n.n3 "Try some inference @@what is this test really about?" rules12.n3 -think
+cwm_test rules12-n.n3 "log:implies rules, iterating" rules12.n3 -think
 
-cwm_test rules13-1.n3 "Try some inference @@what is this test really about?" rules13.n3 -rules
+cwm_test rules13-1.n3 "log:implies rules more complex, with means, once" rules13.n3 -rules
 
-cwm_test rules13-n.n3 "Try some inference" rules13.n3 -think
+cwm_test rules13-n.n3 "log:implies rules more complex, with means, many times" rules13.n3 -think
 
 cwm_test schema1.n3 "Schema validity" daml-ex.n3 invalid-ex.n3 schema-rules.n3 -think
 
-cwm_test schema2.n3 "@@test case description?" daml-ex.n3 invalid-ex.n3 schema-rules.n3 -think -filter=schema-filter.n3
+cwm_test schema2.n3 "Schema validity using filtering out essential output" daml-ex.n3 invalid-ex.n3 schema-rules.n3 -think -filter=schema-filter.n3
 
 # $Log$
-# Revision 1.5  2001-05-10 06:04:23  connolly
+# Revision 1.6  2001-05-21 03:11:59  timbl
+# after changes to namespace and use of this with forall
+#
+# Revision 1.5  2001/05/10 06:04:23  connolly
 # remove more Id stuff
 #
 # Revision 1.4  2001/05/09 15:06:36  connolly
