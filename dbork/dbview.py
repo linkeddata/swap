694e6310d8a0110473c0f7e9187fa7d482c17d50
connolly <connolly>
2002-03-05 22:04:18 +0000

    HTTP interface starting to work...

--- dbork/dbview.py
+++ dbork/dbview.py
@@ -2,6 +2,10 @@
 """
 dbview -- view an SQL DB thru RDF glasses.
 
+Share and Enjoy. Open Source license:
+Copyright (c) 2001 W3C (MIT, INRIA, Keio)
+http://www.w3.org/Consortium/Legal/copyright-software-19980720
+
 hmm... RDF/SQL mapping issue: consider:
 
   select person.name, phone.room from persons,phones
@@ -47,6 +51,63 @@ import MySQLdb # MySQL for Python
 
 
 import RDFSink
+import notation3 # move RDF/XML writer out of notation3
+
+import BaseHTTPServer
+
+
+class DBViewServer(BaseHTTPServer.HTTPServer):
+    def __init__(self, addr, handlerClass, db, base):
+        BaseHTTPServer.HTTPServer.__init__(self, addr, handlerClass)
+        
+        self._db = db
+        self._base = base
+        
+
+class DBViewHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.getRoot()
+        else:
+            self.getView()
+            
+    def getRoot(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        self.wfile.write("""
+        <html xmlns="http://www.w3.org/1999/xhtml">
+        <head><title>RDF query interface@@</title></head>
+        <body>
+        <p>Query:</p>
+        <form action='/dbview'>
+        <input name="q"/>
+        </form>
+        </body>""")
+
+        
+    def getView(self):
+        self.send_response(200, "@@not sure you're winning yet, actually")
+        self.send_header("Content-type", "application/rdf+xml") #@@ cf. RDF Core "what mime type to use for RDF?" issue...
+        self.end_headers()
+
+        addr = self.server._base + self.path
+        
+        sink = notation3.ToRDF(self.wfile, addr)
+
+        ns = addr + '#' #@@hmm...
+        sink.bind('db', (RDFSink.SYMBOL, ns))
+
+        #@@hardcoded
+        fields = ('email', 'given', 'family', 'city', 'state', 'country', 'URL', 'last')
+        tables = ('users', 'techplenary2002')
+        condition = 'users.id=techplenary2002.id'
+        
+        askdb(self.server._db, ns, sink, fields, tables, condition)
+
+
+
 
 g=0
 def something(sink, scope=None, hint='gensym'):
@@ -84,10 +145,27 @@ def askdb(db, ns, sink, fields, tables, condition):
         sink.endAnonymousNode()
 
 
+def testSvc():
+    import sys
+
+    host, port, user, passwd, httpPort = sys.argv[1:6]
+    port = int(port)
+    
+    db=MySQLdb.connect(host=host, port=port,
+                       user=user, passwd=passwd,
+                       db='administration') #@@
+
+    hostPort = ('', int(httpPort))
+    base = "http://127.0.0.1:%s" % httpPort
+    httpd = DBViewServer(hostPort, DBViewHandler, db, base)
+
+    print "Serving HTTP on port", httpPort, "..."
+    httpd.serve_forever()
+
+
+
 
 def testDBView(fp, host, port, user, passwd):
-    import notation3
-    
     #sink = notation3.ToN3(fp.write, 'stdout:')
     sink = notation3.ToRDF(fp, 'stdout:')
     db=MySQLdb.connect(host=host, port=port,
@@ -101,16 +179,24 @@ def testDBView(fp, host, port, user, passwd):
           ('users', 'techplenary2002'),
           'users.id=techplenary2002.id')
     sink.endDoc()
-    
-def test():
+
+
+
+
+def testCLI():
     import sys
     from string import atoi
     
     host, port, user, passwd = sys.argv[1:5]
-    
+
     testDBView(sys.stdout, host, atoi(port), user, passwd)
     
 if __name__ == '__main__':
-    test()
+    #testCLI()
+    testSvc()
+    
 
-
+# $Log$
+# Revision 1.5  2002-03-05 22:04:18  connolly
+# HTTP interface starting to work...
+#
