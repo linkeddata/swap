e19aab1cc71103e7618a55218471518b4f51babe
connolly <connolly>
2002-03-06 17:24:39 +0000

    add pointer to msql doc for mysql-specific schema-interrogation stuff

--- dbork/dbview.py
+++ dbork/dbview.py
@@ -349,7 +349,7 @@ def aboutDB(db, dbaddr, sink):
     sink.bind('rdf', (RDFSink.SYMBOL, str(RDF)))
     
     c = db.cursor()
-    c.execute("show tables")
+    c.execute("show tables") # http://www.mysql.com/documentation/mysql/bychapter/manual_MySQL_Database_Administration.html#SHOW
     while 1:
         row = c.fetchone()
         if not row: break
@@ -590,7 +590,10 @@ if __name__ == '__main__':
 
 
 # $Log$
-# Revision 1.11  2002-03-06 17:20:18  timbl
+# Revision 1.12  2002-03-06 17:24:39  connolly
+# add pointer to msql doc for mysql-specific schema-interrogation stuff
+#
+# Revision 1.11  2002/03/06 17:20:18  timbl
 # (timbl) Changed through Jigsaw.
 #
 # Revision 1.10  2002/03/06 06:37:32  connolly
