e01067625eab9bcb41d9478ef72b860918d3a27d
connolly <connolly>
2002-07-18 05:16:31 +0000

    oops! got ical URI wrong! thx daml validator

--- pim/ical2rdf.pl
+++ pim/ical2rdf.pl
@@ -33,7 +33,7 @@
 
 use strict;
 
-my($ICal_ns) = 'http://www.w3.org/2000/10/swap/ical#';
+my($ICal_ns) = 'http://www.w3.org/2000/10/swap/pim/ical#';
 my($X_ns) = 'http://www.w3.org/2000/01/foo-X@@#'; #@@ get this from command-line? or from PRODID?
 
 my(@stack);
@@ -291,7 +291,10 @@ sub testCamelCase{
 # @@TODO: params
 
 # $Log$
-# Revision 1.2  2002-07-17 20:59:40  connolly
+# Revision 1.3  2002-07-18 05:16:32  connolly
+# oops! got ical URI wrong! thx daml validator
+#
+# Revision 1.2  2002/07/17 20:59:40  connolly
 # using ical:value in stead of rdf:value
 #
 # Revision 1.1  2002/07/17 20:34:55  connolly
