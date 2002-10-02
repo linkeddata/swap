67c08caa74322af883f0921dfd1a4be11eb0b6f3
sandro <sandro>
2002-10-02 18:43:51 +0000

    various improvements

--- testViaOtter.py
+++ testViaOtter.py
@@ -1,28 +1,52 @@
+#  
+
 #
 # total bailing wire for now -- need to move peices into LX
-
-# PYTHONPATH=/home/sandro/2/05/positive-triples/lib/python
 
 import os
 import sys
 
+import rdf
+import toShortNames
+
 axlong=sys.argv[1]
 axname = axlong.split(".")[0]
+axclean = axname + "Cleaned.n3"
 axrdf = axname + ".rdf"
 axP = axname + ".P"
+axOtter = axname + ".otter"
 
 # include swap/util/rdfs-rules.n3
-os.system("cwm %s --flatten --rdf > %s" % (axlong, axrdf))
+os.system("grep -v '#HIDELINE' < %s > %s" % (axlong, axclean))
+os.system("cwm %s --flatten --rdf > %s" % (axclean, axrdf))
 
-import rdf
 ax=rdf.read(axrdf)
-p=rdf.asProlog(ax)
-import toShortNames
+#axclean=[]
+#for t in ax:
+#    skip = 0
+#    for s in t:
+#        if (s.getURI() == "http://www.w3.org/2000/10/swap/log#forAll" or
+#            s.getURI() == "http://www.w3.org/2000/10/swap/log#forSome"):
+#            skip = 1
+#        print s.getURI()
+#    if not skip: axclean.append(t)
+    
+p=rdf.asOtter(ax)
 ps = toShortNames.trans(p)
 f=open(axP, "w")
 f.write(ps)
+f.close()
 
-os.system("xsb -e \"['%s'], ['../LX/recognizeLX.P'], writeKB.  halt.\"" % axP)
+f=open(axOtter, "w")
+f.write("""set(auto).
+formula_list(usable).
+""")
+f.close()
+
+cmd = "xsb --quietload --noprompt --nobanner -e \"['%s'], ['../LX/recognizeLX.P'], writeKB, halt.\" >> %s" % (axP, axOtter)
+#print "PY running:", cmd
+os.system(cmd)
+
 
 # don't actually call otter yet!
 
