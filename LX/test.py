016a59af8993c0348071733cd4063cdd2982103e
sandro <sandro>
2003-01-10 21:33:36 +0000

    added sniff.py

--- LX/test.py
+++ LX/test.py
@@ -9,13 +9,16 @@ import doctest
 import LX
 import LX.engine
 
-for x in [ "LX.expr", "LX.engine.otter" ]:
+for x in [ "LX.expr", "LX.engine.otter", "LX.sniff" ]:
     print "Module", "%-25s" %x,
     __import__(x)
     print "failed %3d of %3d tests." % eval("doctest.testmod(%s)" % x)
     
 # $Log$
-# Revision 1.2  2003-01-08 17:51:22  sandro
+# Revision 1.3  2003-01-10 21:33:36  sandro
+# added sniff.py
+#
+# Revision 1.2  2003/01/08 17:51:22  sandro
 # improved output format
 #
 # Revision 1.1  2003/01/08 17:48:27  sandro
