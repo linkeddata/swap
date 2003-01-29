d8e1af7710d4b2e16a06f0d37403bf1b9a6bc465
sandro <sandro>
2003-01-29 18:54:02 +0000

    added testing ../html module

--- LX/test.py
+++ LX/test.py
@@ -8,14 +8,19 @@ __version__ = "$Revision$"
 import doctest
 import LX
 import LX.engine
+import LX.language
+import html
 
-for x in [ "LX.expr", "LX.engine.otter", "LX.sniff" ]:
+for x in [ "LX.expr", "LX.engine.otter", "LX.sniff", "html" ]:
     print "Module", "%-25s" %x,
     __import__(x)
     print "failed %3d of %3d tests." % eval("doctest.testmod(%s)" % x)
     
 # $Log$
-# Revision 1.3  2003-01-10 21:33:36  sandro
+# Revision 1.4  2003-01-29 18:54:02  sandro
+# added testing ../html module
+#
+# Revision 1.3  2003/01/10 21:33:36  sandro
 # added sniff.py
 #
 # Revision 1.2  2003/01/08 17:51:22  sandro
