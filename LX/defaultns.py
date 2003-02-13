04ecee5e75a6fa4b356b7afe2e9c130dd2df0f81
sandro <sandro>
2003-02-13 19:49:51 +0000

    first draft

---
+++ LX/defaultns.py
@@ -0,0 +1,15 @@
+
+from LX.namespace import Namespace
+
+# See http://www.w3.org/TR/rdf-syntax-grammar/#section-Namespace
+#  (we'll want special handling of _n with a subclass)
+rdfns  = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns",
+                   ["type", "nil", "first", "rest"])
+#
+# See http://www.w3.org/TR/rdf-schema/
+rdfsns = Namespace("http://www.w3.org/2000/01/rdf-schema",
+                   ["Resource", "Class", "Datatype", "XMLLiteral"])
+#
+lx2ns  = Namespace("http://www.w3.org/2002/08/LX/RDF/v2", strict=0)
+lxns   = Namespace("http://www.w3.org/2003/02/04/LX", strict=0)
+
