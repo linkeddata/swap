ba6455e88d44618240808ec04469df5d6b27e5b4
sandro <sandro>
2003-02-13 17:20:19 +0000

    moved URI-related stuff over to uri.py

--- LX/__init__.py
+++ LX/__init__.py
@@ -5,38 +5,32 @@ We import most modules here, so users can just use LX.Foo instead of
 LX.foo.Foo.  That means we have to be more careful about naming
 conflicts, but it also gives us more freedom to move code between
 files without affecting the API.
+
 """
 __version__ = "$Revision$"
 # $Id$
 
-# To allow "from LX import *", though I'm not sure when/why one would
+# To allow "from LX import *", though I'm not sure when one would
 # want that.
-__all__ = ["operator", "language", "formula", "term", "kb"] 
+__all__ = ["language", "engine", "kb", "expr", "logic", "fol", "rdf",
+           "namespace", "uri" ] 
 
-# Let people use LX.Formula instead of LX.formula.Formula, etc, while
+# Let people use LX.Expr instead of LX.expr.Expr, etc, while
 # having these parts in separate files
-from LX.operator import *
-from LX.term import *
-from LX.formula import *
+from LX.uri import *
 from LX.kb import *
+from LX.expr import *
+from LX.logic import *
 from LX.describer import *
-
 from LX.namespace import *
-rdfns  = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns",
-                   ["type", "nil", "first", "rest"])
-lxns   = Namespace("http://www.w3.org/2002/08/LX/RDF/v2", strict=0)
+from LX.rdf import *
 
-
-class URIRef2(str):
-    def __str__(self):
-        return "<"+self+">"
-
-class StrLit(str):
-    def __str__(self):
-        return "\""+self+"\""
 
 # $Log$
-# Revision 1.4  2003-01-29 06:09:18  sandro
+# Revision 1.5  2003-02-13 17:20:19  sandro
+# moved URI-related stuff over to uri.py
+#
+# Revision 1.4  2003/01/29 06:09:18  sandro
 # Major shift in style of LX towards using expr.py.  Added some access
 # to otter, via --check.  Works as described in
 # http://lists.w3.org/Archives/Public/www-archive/2003Jan/0024
@@ -48,7 +42,8 @@ class StrLit(str):
 # passes many more tests, esp handling of variables
 #
 # Revision 1.2  2002/08/29 16:39:55  sandro
-# fixed various early typos and ommissions; working on logic bug which is manifesting in description loops
+# fixed various early typos and ommissions; working on logic bug which
+# is manifesting in description loops  
 #
 # Revision 1.1  2002/08/29 11:00:46  sandro
 # initial version, mostly written or heavily rewritten over the past
