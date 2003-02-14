77f504109e560c4788a78bf633e0836dfdae163f
sandro <sandro>
2003-02-14 22:21:10 +0000

    Got it mostly working again, so data can roundtrip between llyn and lx.Exprs.  Still buggy about uris, rdf(s,p,o) vs p(s,o), some logic constructs -> llyn, and multiple interpretations. One wonders if it wouldn't make more sense to go via n3.

--- LX/engine/llynInterface.py
+++ LX/engine/llynInterface.py
@@ -11,16 +11,19 @@ def _test():
 if __name__ == "__main__": _test()
 
 import llyn
-from llyn import PRED, SUBJ, OBJ, RDFSink, CONTEXT
-import LX.fol
+from llyn import PRED, SUBJ, OBJ, CONTEXT
+import RDFSink
+import LX.logic
+import LX.expr
+import LX.uri
 
 
 ################################################################
 
 def toLX(store, formula, maxDepth=4, kb=None, kbMode=0, terms={}):
     """Convert a llyn.formula to an LX.Formula and return it, or
-    to one or more LX.Formulas and add them to an LX.KB; also convert
-    a non-formula into an LX.Term.
+    to one or more LX.expr.Expr's and add them to an LX.KB; also convert
+    a non-formulas.
 
     If a kb is specified, the conjoined parts of the formula (or
     just the formula itself, if there are no conjoined parts) will
@@ -38,8 +41,9 @@ def toLX(store, formula, maxDepth=4, kb=None, kbMode=0, terms={}):
             return terms[formula]
         except KeyError:
             # then it's a constant!
-            toLXVar(store, formula, terms, makeWith=LX.fol.Constant)
-            kb.interpret(terms[formula], LX.URIRef2(formula.uriref()))
+            toLXVar(store, formula, terms, makeWith=LX.logic.Constant)
+            kb.interpret(terms[formula],     ### bug:  need to know position in triple!
+                         LX.uri.DescribedThing(formula.uriref()))
             return terms[formula]
 
     #iTerms = terms.copy()
@@ -47,8 +51,8 @@ def toLX(store, formula, maxDepth=4, kb=None, kbMode=0, terms={}):
 
     if kbMode: assert(kb != None)
 
-    for v in formula.existentials(): toLXVar(store, v, iTerms, makeWith=LX.fol.ExiVar)
-    for v in formula.universals():   toLXVar(store, v, iTerms, makeWith=LX.fol.UniVar)
+    for v in formula.existentials(): toLXVar(store, v, iTerms, makeWith=LX.logic.ExiVar)
+    for v in formula.universals():   toLXVar(store, v, iTerms, makeWith=LX.logic.UniVar)
 
     result = None
     if maxDepth < 0:
@@ -61,17 +65,18 @@ def toLX(store, formula, maxDepth=4, kb=None, kbMode=0, terms={}):
             # as in   {   } log:forall :x.
             this = toLX(store, s[SUBJ], maxDepth-1, kb=kb, terms=iTerms)
         elif s[PRED] is store.implies:
-            this = LX.fol.IMPLIES(toLX(store, s[SUBJ], maxDepth-1, kb=kb, terms=iTerms),
+            this = LX.logic.IMPLIES(toLX(store, s[SUBJ], maxDepth-1, kb=kb, terms=iTerms),
                                   toLX(store, s[OBJ], maxDepth-1, kb=kb, terms=iTerms))
         elif s[PRED] is store.means:
-            this = LX.fol.MEANS(toLX(store, s[SUBJ], maxDepth-1, kb=kb, terms=iTerms),
+            this = LX.logic.MEANS(toLX(store, s[SUBJ], maxDepth-1, kb=kb, terms=iTerms),
                                 toLX(store, s[OBJ], maxDepth-1, kb=kb, terms=iTerms))
         elif s[PRED] is store.type and s[OBJ] is store.Truth:
             this = toLX(store, s[SUBJ], maxDepth-1, kb=kb, terms=iTerms)
         elif s[PRED] is store.type and s[OBJ] is store.Falsehood:
-            this = LX.fol.NOT(toLX(store, s[SUBJ], maxDepth-1, kb=kb, terms=iTerms))
+            this = LX.logic.NOT(toLX(store, s[SUBJ], maxDepth-1, kb=kb, terms=iTerms))
         else:
-            this = LX.fol.RDF(toLX(store, s[SUBJ], maxDepth-1, kb=kb, terms=iTerms),
+            # use position for #-smarts here; lower order here....
+            this = LX.logic.RDF(toLX(store, s[SUBJ], maxDepth-1, kb=kb, terms=iTerms),
                               toLX(store, s[PRED], maxDepth-1, kb=kb, terms=iTerms),
                               toLX(store, s[OBJ], maxDepth-1, kb=kb, terms=iTerms))
             
@@ -81,15 +86,15 @@ def toLX(store, formula, maxDepth=4, kb=None, kbMode=0, terms={}):
             if result is None:
                 result = this
             else:
-                result = LX.fol.AND(result, this)
+                result = LX.logic.AND(result, this)
 
     if kbMode:
         for v in formula.existentials():  kb.exivars.append(iTerms[v])
         for v in formula.universals():    kb.univars.append(iTerms[v])
         return None
     else:    
-        for v in formula.existentials(): result = LX.fol.EXISTS(iTerms[v], result)
-        for v in formula.universals():   result = LX.fol.FORALL(iTerms[v], result)
+        for v in formula.existentials(): result = LX.logic.EXISTS(iTerms[v], result)
+        for v in formula.universals():   result = LX.logic.FORALL(iTerms[v], result)
         return result
 
 def toLXVar(store, term, vars, makeWith=0):
@@ -102,40 +107,73 @@ def toLXVar(store, term, vars, makeWith=0):
 
 ################################################################
 
-## def addLXKB(store, context, kb):
-##     "Add all the formulas in an LX.KB to the store"
-##     terms = {}
-##     for term in kb.exivars:
-##         # Oh, maybe we should use RDFSink.newExistential, I see now.
-##         # but.... it doesn't allow us to give our own fragment name.
-##         if term.value:
-##             n = store.intern((RDFSink.SYMBOL, term.value),)
-##         else:
-##             n = store.intern((RDFSink.ANONYMOUS, store._genPrefix+term.name),)
-##         terms[term] = n
-##         store.storeQuad([context, store.forSome, context, n])
-##     for term in kb.univars:
-##         if term.value:
-##             n = store.intern((RDFSink.SYMBOL, term.value),)
-##         else:
-##             n = store.intern((RDFSink.ANONYMOUS, store._genPrefix+term.name),)
-##         terms[term] = n
-##         store.storeQuad([context, store.forAll, context, n])
-##     for f in kb:
-##         addLXFormula(store, context, f, terms)
+import re
+scope = {
+    ("legal",): re.compile(r"^[a-zA-Z0-9_]+$"),
+    ("hint",): re.compile(r"(?:\/|#)([a-zA-Z0-9_]*)/?$")
+    }
 
-## def addLXFormula(store, context, lxFormula, terms={}):
-##     """Add an LX.Formula to the store.
+def addLXKB(store, context, kb):
+    "Add all the formulas in an LX.KB to the store"
+    terms = {}
+    for term in kb.exivars:
+        # Oh, maybe we should use RDFSink.newExistential, I see now.
+        # but.... it doesn't allow us to give our own name hint.
+        n = store.intern((RDFSink.ANONYMOUS, store._genPrefix+term.getNameInScope(scope)),)
+        terms[term] = n
+        store.storeQuad([context, store.forSome, context, n])
+    for term in kb.univars:
+        n = store.intern((RDFSink.ANONYMOUS, store._genPrefix+term.getNameInScope(scope)),)
+        terms[term] = n
+        store.storeQuad([context, store.forAll, context, n])
+    for f in kb:
+        addLXFormula(store, context, f, terms, kb)
 
-##     terms maps from LX.Term to RDFSink pairs
-##     """
-##     if lxFormula.operator is LX.ATOMIC_SENTENCE:
-##         t = [None, None, None, None]
-##         t[CONTEXT] = context
-##         for (index, term) in zip((SUBJ, PRED, OBJ), lxFormula.operands):
-##             try:
-##                 sym = terms[term]
-##             except KeyError:
+def addLXFormula(store, context, expr, terms={}, kb=None):
+    """Add an LX.Formula to the store.
+
+    terms maps from LX.AtomicExpr to RDFSink pairs
+    """
+
+    if expr.function is LX.logic.IMPLIES:
+        t = [None, None, None, None]
+        left = store.newFormula()
+        addLXFormula(store, left, expr.args[0], terms, kb)
+        left = left.close()
+        right = store.newFormula()
+        addLXFormula(store, right, expr.args[1], terms, kb)
+        right = right.close()
+        t[SUBJ] = left
+        t[PRED] = store.implies
+        t[OBJ] = right
+        t[CONTEXT] = context
+        store.storeQuad(t)
+    elif expr.function is LX.logic.NOT:
+        t = [None, None, None, None]
+        left = store.newFormula()
+        addLXFormula(store, left, expr.args[0], terms, kb)
+        left = left.close()
+        t[SUBJ] = left
+        t[PRED] = store.type
+        t[OBJ] = store.Falsehood
+        t[CONTEXT] = context
+        store.storeQuad(t)
+    elif expr.function is LX.logic.RDF:
+        t = [None, None, None, None]
+        t[CONTEXT] = context
+        for (index, term) in zip((SUBJ, PRED, OBJ), expr.args):
+            try:
+                sym = terms[term]
+            except KeyError:
+                #print "What llyn tuple to use for", term, "index", index
+                uri = None
+                interps = kb.getInterpretations(term)
+                assert(len(interps) == 1)
+
+                #rint "... Interp:", interps[0]
+                uri = interps[0].uriOfDescription
+                sym = store.intern((RDFSink.SYMBOL, uri),)
+                    
 ##                 if isinstance(term, LX.URIRef):
 ##                     sym = store.intern((RDFSink.SYMBOL, term.value),)
 ##                 elif isinstance(term, LX.Variable):
@@ -147,14 +185,20 @@ def toLXVar(store, term, vars, makeWith=0):
 ##                 else:
 ##                     msg = "Conversion of %s's not implemented" % term.__class__
 ##                     raise RuntimeError, msg
-##             t[index] = sym
-##         store.storeQuad(t)
-##     else: 
-##         raise RuntimeError, "Can only convert atomic formula yet"
+            t[index] = sym
+        store.storeQuad(t)
+    else: 
+        raise RuntimeError, "Can only convert atomic formula yet, not %s" % expr
 
 
 # $Log$
-# Revision 1.3  2003-01-29 06:09:18  sandro
+# Revision 1.4  2003-02-14 22:21:10  sandro
+# Got it mostly working again, so data can roundtrip between
+# llyn and lx.Exprs.  Still buggy about uris, rdf(s,p,o) vs p(s,o),
+# some logic constructs -> llyn, and multiple interpretations.
+# One wonders if it wouldn't make more sense to go via n3.
+#
+# Revision 1.3  2003/01/29 06:09:18  sandro
 # Major shift in style of LX towards using expr.py.  Added some access
 # to otter, via --check.  Works as described in
 # http://lists.w3.org/Archives/Public/www-archive/2003Jan/0024
