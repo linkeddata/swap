afe7c0d944be88f2f1501e5a40b6bbe599e57fc2
danbri <danbri>
2003-03-23 19:56:29 +0000

    Hacking towards a setup.py that works (but failing; confused about which dirs live where).

--- packages/debian/setup.py
+++ packages/debian/setup.py
@@ -1,14 +1,42 @@
 #!/usr/bin/env python
+# borrowing liberally from Eikeon's setup.py for rdflib
+# danbri@w3.org
 
-import sys
-sys.stderr=sys.stdout
-print """Setting up Cwm... (or not, in this case)"""
+from distutils.sysconfig import get_python_lib
+from os import rename
+from os.path import join, exists
+from time import time
 
-for file in ('lib/*.py'):
-  print """File is """ + file
+lib_dir = get_python_lib()
+swap_dir = join(lib_dir, "lib")
+print "swap dir: "+swap_dir
 
 
-# http://www.python.org/doc/1.6/dist/simple-example.html
-# compare:
-# 4suite installer, 
-# http://easynews.dl.sourceforge.net/sourceforge/foursuite/4Suite-0.12.0a3-doc.tar.gz
+if exists(swap_dir):
+    backup = "%s-%s" % (swap_dir, int(time()))
+    print "Renaming previously installed rdflib to: \n  %s" % backup
+    rename(swap_dir, backup)
+
+
+# Install SWAP
+from distutils.core import setup
+#from swap import __version__
+__version__='0.31417'
+setup(
+    name = 'SWAP/Cwm',
+    version = __version__,
+    description = "Semantic Web Area for Play",
+    author = "TimBL, Dan Connolly and contributors",
+    author_email = "timbl@w3.org",
+    maintainer = "Tim Berners-Lee",
+    maintainer_email = "timbl@w3.org",
+    url = "http://www.w3.org/2000/10/swap/",
+    package_dir = {'swap': 'swap-cwm-0.9/lib/swap'},
+    packages = ['swap.RDFSink']
+
+    )
+    # todo, figure out which other modules are in public APIs
+    # --danbri
+#
+#		'swap.RDFSink',
+#		'swap.llyn'],
