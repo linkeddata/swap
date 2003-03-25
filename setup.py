3c6e73b7ee63a821d4e373861e7120694cc0a335
danbri <danbri>
2003-03-25 21:37:36 +0000

    added notes, url etc

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 # borrowing liberally from Eikeon's setup.py for rdflib
-# danbri@w3.org
-
+# an attempt at a setup.py installer for Cwm/SWAP
+# version: $Id$
+# by Dan Brickley <danbri@w3.org>
+#
 # STATUS: not working yet
+# 
+# notes:
+# http://esw.w3.org/t/view/ESW/CwmTips
+
+# current output / error:
+#	sudo ./setup.py install
+#	swap dir: /usr/lib/python2.2/site-packages/swap
+#	running install
+#	running build
+#	running build_py
+#	error: package directory 'swap/swap/cwm' does not exist
+
 
 from distutils.sysconfig import get_python_lib
 from os import rename
@@ -10,7 +24,7 @@ from os.path import join, exists
 from time import time
 
 lib_dir = get_python_lib()
-swap_dir = join(lib_dir, "lib")
+swap_dir = join(lib_dir, "swap")
 print "swap dir: "+swap_dir
 
 
@@ -18,6 +32,7 @@ if exists(swap_dir):
     backup = "%s-%s" % (swap_dir, int(time()))
     print "Renaming previously installed rdflib to: \n  %s" % backup
     rename(swap_dir, backup)
+
 
 
 # Install SWAP
@@ -33,12 +48,17 @@ setup(
     maintainer = "Tim Berners-Lee",
     maintainer_email = "timbl@w3.org",
     url = "http://www.w3.org/2000/10/swap/",
-     packages = ['swap']
+    package_dir = {'': 'swap'},
+    packages = ['swap.cwm',
+		'swap.RDFSink',
+		'swap.llyn'],
    )
     # todo, figure out which other modules are in public APIs
     # --danbri
-#,'swap.cwm','swap.RDFSink','swap.llyn'],
 
+
+
+#,'swap.cwm','swap.RDFSink','swap.llyn'],
 #		'swap.RDFSink',
 #		'swap.llyn'],
 #    packages = ['swap.cwm',
