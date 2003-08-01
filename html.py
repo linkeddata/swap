b2b409f56c8cf9bd91579cd7e074d228d6e9e781
sandro <sandro>
2003-08-01 15:40:46 +0000

    simpler API; actually changed Apr 29 but not comitted

--- html.py
+++ html.py
@@ -5,6 +5,41 @@ keep the page well-formed, valid, and generally pleasant.  Why I'm
 writing this in 2003 instead of 1993 is anyone's guess.  I must be
 bored or procrastinating.
 
+Write HTML In 7 Seconds:
+
+   >>> from html import *
+   >>> d = Document()
+   >>> d.head << title("Web Site Story")
+   >>> d << h1("Web Site Story")
+   >>> d << p("To be continued...")
+   >>> print prefixEveryLine("* ", d)
+   * <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+   *           "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+   * <html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+   *   <head><title>Web Site Story</title></head>
+   * 
+   *   <body>
+   *     <h1>Web Site Story</h1>
+   * 
+   *     <p>To be continued...</p>
+   *   </body>
+   * </html>
+   *
+
+
+[[  document should follow
+       d.printWhenReady(1)
+       d.start(p)
+       d << "text"
+       d.end()
+    streaming style, I think, and
+    if you want mutable fragments
+    you make and use them before
+    serializing....
+]]
+
+More Thorough:
+
    >>> from html import *
    >>> print span("Hello, World!")
    <span>Hello, World!</span>
@@ -29,15 +64,20 @@ The content is often a sequence...
    >>> print Element("span", ["Hello, ", "World!"], inline=1)
    <span>Hello, World!</span>
 
+but that's deprectaed -- now use:
+
+   >>> print Element("span", "Hello, ", "World!", inline=1)
+   <span>Hello, World!</span>
+
 Attributes are supported, of course...
 
-   >>> e = Element("span", ["Hello, ", "World!"], { "class": "bright" }, inline=1)
+   >>> e = Element("span", "Hello, ", "World!", class_="bright", inline=1)
    >>> print e
    <span class="bright">Hello, World!</span>
 
 append() appends to the content...
 
-   >>> e.append(" Goodnight, Moon! ")
+   >>> dummy = e.append(" Goodnight, Moon! ")
    >>> print e
    <span class="bright">Hello, World! Goodnight, Moon! </span>
 
@@ -57,8 +97,8 @@ Elements may be nested, of course...
 The Document class handle doctype, html/head/body.
 
    >>> d = Document()
-   >>> d.head.append(title("Test Document"))
-   >>> d.append(h1("Test Document"))
+   >>> dummy = d.head.append(title("Test Document"))
+   >>> dummy = d.append(h1("Test Document"))
    >>> print prefixEveryLine("* ", d)
    * <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
    *           "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -139,8 +179,10 @@ class Document(Streamer):
        if isinstance(content, Element):
            self.body.append(content)
        else:
-           self.body.append(div(content))
-       return self
+           # print "auto diving", content
+           #self.body.append(div(content))
+           self.body.append(content)
+       #return self
 
    def __lshift__(self, content):
        """Convenience syntax for append, with no parens to balance."""
@@ -148,12 +190,34 @@ class Document(Streamer):
 
 class Element(Streamer):
 
-    def __init__(self, tag, content=None, attrs=None, inline=0):
+    def __init__(self, tag, *content, **kwargs):
         self.tag=tag
-        self.content=content
-        self.attrs=attrs
-        if self.attrs is None: self.attrs = {}
-        self.inline = inline
+
+        # pull out the real keyword args first
+        if "inline" in kwargs:
+            self.inline = kwargs["inline"]
+            del kwargs["inline"]
+        else:
+            self.inline = 0
+            
+        if "attrs" in kwargs:
+            # old style attrs
+            self.attrs = kwargs["attrs"]
+            del kwargs["attrs"]
+        else:
+            self.attrs = kwargs
+            
+        self.content=[]
+        for x in content:
+            if isinstance(x, list):
+                # flatten one layer of nested lists (deprecated)
+                self.content.extend(x)
+            elif isinstance(x, dict):
+                # let attrs be passed without a keyword (deprecated)
+                self.attrs.update(x)
+            else:
+                self.content.append(x)
+
 
     prefixLength = 2
 
@@ -209,7 +273,8 @@ class Element(Streamer):
         except AttributeError:
             self.content = [ self.content ]
             self.content.append(content)
-        return self  # allows chaining    x.append(y).append(z)
+        #return self  # allows chaining    x.append(y).append(z)
+        # (but then we get back a value we rarely use.  ugh)
 
     def __lshift__(self, content):
        """Convenience syntax for append, with no parens to balance."""
@@ -262,8 +327,9 @@ class Flow:
     * 
     """
     def __getattr__(self, name):
-        def func(content="", **kw):
-            return Element(name, content, attrs=kw)
+        def func(*content, **kw):
+            kw["inline"] = 0
+            return apply(Element, [name]+list(content), kw)
         return func
 
 class Inline:
@@ -274,8 +340,9 @@ class Inline:
     <span class="authorName">Spanned Text</span>
     """
     def __getattr__(self, name):
-        def func(content="", **kw):
-            return Element(name, content, attrs=kw, inline=1)
+        def func(*content, **kw):
+            kw["inline"] = 1
+            return apply(Element, [name]+list(content), kw)
         return func
 
 """Foo"""
@@ -319,7 +386,10 @@ if __name__ =='__main__':
     print doctest.testmod(html) 
 
 # $Log$
-# Revision 1.4  2003-04-29 04:46:57  sandro
+# Revision 1.5  2003-08-01 15:40:46  sandro
+# simpler API; actually changed Apr 29 but not comitted
+#
+# Revision 1.4  2003/04/29 04:46:57  sandro
 # added clever << operator (now where have I seen that before?)
 #
 # Revision 1.3  2003/02/18 17:02:05  sandro
