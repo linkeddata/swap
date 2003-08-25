53281daa1ebc9e2db8144f50bae53a4d7b73872c
sandro <sandro>
2003-08-25 18:21:44 +0000

    rolled defaultns.py into namespace.py

--- LX/namespace.py
+++ LX/namespace.py
@@ -1,19 +1,33 @@
 """A convenience for handling RDF-style namespaces
 
-TODO: load names from web?
+Includes the "ns" object which gives you handy names for
+commonly-used RDF namespaces.
 
-See modules defaultns.py for some uses.
+>>> from LX.namespace import ns
+>>> ns.rdf.type.uri
+'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+>>> ns.dc.author.uri
+'http://purl.org/dc/elements/1.1/author'
+
+TODO: option for loading names from web
+TODO: add in details for "strict" for "ns"
 
 """
 __version__ = "$Revision$"
 # $Id$
 
-#import LX.uri
 import LX.logic
 
 class NamespaceCluster:
-    """
-    Use like ns.rdf.type 
+    """An object whose attributes (eg rdf, rdfs, foaf, dc, ...) are
+    short names for Namespace objects.  
+    
+    >>> from LX.namespace import Namespace, NamespaceCluster
+    >>> ns = NamespaceCluster()
+    >>> ns.app1 = Namespace("http://example.com/ns1", strict=0)
+    >>> ns.app1.foo.uri
+    'http://example.com/ns1#foo'
+
     """
     def inverseLookup(self, term):
         uri = term.uri
@@ -43,13 +57,13 @@ class Namespace:
     File "<string>", line 1, in ?
     File "/home/sandro/0/10/swap/LX/namespace.py", line 46, in __getattr__
     raise AttributeError, msg
-    AttributeError: No name sandro declared for namespace http://www.example.com/friends (in strict mode)
+    AttributeError: No name sandro declared for namespace http://www.example.com/friends# (in strict mode)
     >>> ns1.add("sandro")
-    >>> print ns1.sandro
-    <http://www.example.com/friends#sandro>
+    >>> ns1.sandro.uri
+    'http://www.example.com/friends#sandro'
     >>> ns2 = Namespace("http://www.example.com/friends", strict=0)
-    >>> print ns2.sandro
-    <http://www.example.com/friends#sandro>
+    >>> ns2.sandro.uri
+    'http://www.example.com/friends#sandro'
 
     """
     def __init__(self, uri, initialNames=[], strict=1, shortForm=None):
@@ -80,9 +94,27 @@ class Namespace:
         self.__dict__[name] = result
         return result
 
+ns = NamespaceCluster()
+ns.rdf  = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns",
+                   ["type", "nil", "first", "rest", "XMLLiteral"])
+ns.rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema",
+                   ["Resource", "Class", "Datatype", "seeAlso"])
+ns.dc = Namespace("http://purl.org/dc/elements/1.1/", strict=0)
+ns.foaf = Namespace("http://xmlns.com/foaf/0.1/", strict=0)
+ns.log = Namespace("http://www.w3.org/2000/10/swap/log#", strict=0)
+ns.owl = Namespace("http://www.w3.org/2002/07/owl#", strict=0)
+ns.lx   = Namespace("http://www.w3.org/2003/02/04/LX", strict=0)
+
+if __name__ == "__main__":
+    import doctest, sys
+    doctest.testmod(sys.modules[__name__])
+
 
 # $Log$
-# Revision 1.6  2003-08-22 20:49:07  sandro
+# Revision 1.7  2003-08-25 18:21:44  sandro
+# rolled defaultns.py into namespace.py
+#
+# Revision 1.6  2003/08/22 20:49:07  sandro
 # generalized ns and reconstructor, for second use (2003/08/owl-systems)
 #
 # Revision 1.5  2003/08/20 09:26:48  sandro
