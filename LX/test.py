363b43e4dab863d984d202c3da7922b0cc701c74
sandro <sandro>
2003-09-17 16:09:58 +0000

    also run any functions starting with __test...

--- LX/test.py
+++ LX/test.py
@@ -13,10 +13,18 @@ for x in LX.__all__:
     print "Module", "%-25s" %x,
     lx = __import__("LX."+x)
     m = getattr(lx, x)
-    print "failed %3d of %3d tests." % doctest.testmod(m)
+    print "failed %3d of %3d doctest lines." % doctest.testmod(m)
+
+    for (name, value) in m.__dict__.iteritems():
+        if name.startswith("__test") and callable(value):
+            print "...running",x+"."+name+"()"
+            apply(value, [])
     
 # $Log$
-# Revision 1.6  2003-02-13 19:25:04  sandro
+# Revision 1.7  2003-09-17 16:09:58  sandro
+# also run any functions starting with __test...
+#
+# Revision 1.6  2003/02/13 19:25:04  sandro
 # Use LX.__all__ to get the module names automagically
 #
 # Revision 1.5  2003/02/03 17:20:40  sandro
