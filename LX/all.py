8d71c679a571b0874c346c3eed72fa3b145f8bfa
sandro <sandro>
2003-09-17 16:08:29 +0000

    made simple & automatic

--- LX/all.py
+++ LX/all.py
@@ -7,17 +7,15 @@ do "from LX.all import *".
 __version__ = "$Revision$"
 # $Id$
 
-from LX import *
-from LX.uri import *
-from LX.kb import *
-from LX.expr import *
-from LX.logic import *
-from LX.describer import *
-from LX.namespace import *
-from LX.rdf import *
-from LX.defaultns import *
+import LX
+
+for x in LX.__all__:
+    exec "from LX."+x+" import *"
 
 # $Log$
-# Revision 1.1  2003-02-13 19:42:49  sandro
+# Revision 1.2  2003-09-17 16:08:29  sandro
+# made simple & automatic
+#
+# Revision 1.1  2003/02/13 19:42:49  sandro
 # first version
 #
