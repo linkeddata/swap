8f08518164279ea03cbced0244ad1893653987aa
connolly <connolly>
2004-03-09 23:29:40 +0000

    removed non-ascii chars from copyright blurb reformatted changelog to 79chars

--- pim/toIcal.py
+++ pim/toIcal.py
@@ -31,12 +31,12 @@ NOTE: see earlier work:
 see changelog at end
 
 
-Copyright ©  2000-2004 World Wide Web Consortium, (Massachusetts Institute
-of Technology, European Research Consortium for Informatics and Mathematics,
-Keio University). All Rights Reserved. This work is distributed under the
-W3C® Software License [1] in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- FOR A PARTICULAR PURPOSE.
+Copyright (C) 2000-2004 World Wide Web Consortium, (Massachusetts
+Institute of Technology, European Research Consortium for Informatics
+and Mathematics, Keio University). All Rights Reserved. This work is
+distributed under the W3C(R) Software License [1] in the hope that it
+will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 """
 
@@ -293,17 +293,12 @@ if __name__ == '__main__':
 
 
 # $Log$
-# Revision 2.14  2004-03-06 20:39:40  timbl
-# See http://www.w3.org/2000/10/swap/doc/changes.html for details
-# - Regresssion test incorporates the RDF Core Positive Parser Tests except XMLLiteral & reification
-# - xml:base support was added in the parser.
-# - Use the --rdf=R flag to allow RDF to be parsed even when there is no enveloping <rdf:RDF> tag
-# - nodeid generated on RDF output
-# - Automatically generated terms with no URIs sort after anything which has a URI.
-# - Namespace prefix smarts on output - default ns used for that most frequently used.
-# - suppresses namespace prefix declarations which are not actually needed in the output.
-# - Cwm will also make up prefixes when it needs them for a namespace, and none of the input data uses one.-
-# - Will not use namespace names for URIs which do not have a "#". Including a "/" in the flags overrides.
+# Revision 2.15  2004-03-09 23:29:40  connolly
+# removed non-ascii chars from copyright blurb
+# reformatted changelog to 79chars
+#
+# Revision 2.14  2004/03/06 20:39:40  timbl
+# added copyright blurb.
 #
 # Revision 2.13  2004/02/23 16:50:54  connolly
 # schema-based rewrite. might have regressed a bit.
@@ -318,10 +313,12 @@ if __name__ == '__main__':
 # todo status support; no test; blech
 #
 # Revision 2.5  2004/01/29 21:28:13  timbl
-# Changed ONE newline to a CRLF, suspect many more should be changed. iCal needs both
+# Changed ONE newline to a CRLF, suspect many more should be changed.
+# iCal needs both
 #
 # Revision 2.4  2004/01/29 21:09:16  timbl
-# Added DTSTART and UID to events. iCal needs DTSTART it seems. Fixed VALUE=URI format.
+# Added DTSTART and UID to events. iCal needs DTSTART it seems.
+# Fixed VALUE=URI format.
 #
 # Revision 2.3  2004/01/29 19:41:57  timbl
 # minor fixes
