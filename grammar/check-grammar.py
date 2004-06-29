24e5839f4e2d989a682d4d246e3862a3a581e0a3
timbl <timbl>
2004-06-29 18:48:20 +0000

    Generates yacc file from selectors

--- grammar/check-grammar.py
+++ grammar/check-grammar.py
@@ -12,9 +12,6 @@ from time import clock
 import re
 
 
-already = []
-agenda = []
-errors = []
 
 
 branchTable = {}
@@ -25,7 +22,83 @@ def recordError(str):
     errors.append(str)
     str2 =  "##### ERROR:  " + str
     return str2
+
+def deColonise(s):
+    t = ""
+    for ch in s:
+	if ch == ":": ch = "_"
+	t = t + ch
+    return t
+
+def toYacc(x, tokenRegexps):
+    if isinstance(x, Literal):
+	return "'" + str(x.value()) + "'"  # @@@ Escaping
+    if x in tokenRegexps:
+	return deColonise(`x`).upper()
+    return deColonise(`x`)
+
+	
+def yaccConvert(yacc, top, tokenRegexps):
+    global already, agenda, errors
+    already = []
+    agenda = []
+    errors = []
+    for x in tokenRegexps:
+	yacc.write("%%token %s\n" % toYacc(x, tokenRegexps))
+    yacc.write("\n")
+    yacc.write("%%\n")
+    yaccProduction(yacc, top, tokenRegexps)
+    while agenda:
+	x = agenda[0]
+	agenda = agenda[1:]
+	already.append(x)
+	yaccProduction(yacc, x, tokenRegexps)
+    yacc.write("eof:  /* empty */; \n")
+    yacc.write("%%\n")
     
+def yaccProduction(yacc, lhs,  tokenRegexps):
+    if lhs is BNF.void:
+	print "\nvoid"
+	return
+    if lhs is BNF.eof:
+	print "\nEOF"
+	return
+    if isinstance(lhs, Literal):
+	literalTerminals[lhs.value()] = 1
+#    	print "\nLiteral %s" %(lhs)
+	return
+
+    rhs = g.the(pred=BNF.matches, subj=lhs)
+    if rhs != None:
+	print "\nToken %s matches regexp %s" %(lhs, rhs)
+#	tokenRegexps[lhs] = re.compile(rhs.value())
+	return
+    rhs = g.the(pred=BNF.mustBeOneSequence, subj=lhs)
+    if rhs == None:
+	print recordError("No definition of " + `lhs`)
+	raise ValueError("No definition of %s  in\n %s" %(`lhs`, `g`))
+    options = rhs
+    print "\nProduction %s :: %s  ie %s" %(`lhs`, `options` , `options.value()`)
+    yacc.write("\n%s:" % toYacc(lhs, tokenRegexps))
+
+    branches = g.each(subj=lhs, pred=BNF.branch)
+    first = 1
+    for branch in branches:
+	if not first:
+	    yacc.write("\t|\t")
+	first = 0
+	option = g.the(subj=branch, pred=BNF.sequence)
+	print "\toption: "+`option.value()`
+	yacc.write("\t")
+	if option.value() == [] and yacc: yacc.write(" /* empty */")
+	for part in option:
+	    if part not in already and part not in agenda: agenda.append(part)
+	    yacc.write(" %s" % toYacc(part, tokenRegexps))
+	yacc.write("\n")
+    yacc.write("\t;\n")
+	
+########################################## end of yacc converter
+
 literalTerminals = {}
 def doProduction(lhs):
     global branchTable
@@ -35,7 +108,6 @@ def doProduction(lhs):
     if lhs is BNF.eof:
 	print "\nEOF"
 	return
-
     if isinstance(lhs, Literal):
 	literalTerminals[lhs.value()] = 1
 #    	print "\nLiteral %s" %(lhs)
@@ -58,7 +130,6 @@ def doProduction(lhs):
 #	raise RuntimeError("No definition of %s  in\n %s" %(`lhs`, `g`))
     options = rhs
     print "\nProduction %s :: %s  ie %s" %(`lhs`, `options` , `options.value()`)
-
     succ = g.each(subj=lhs, pred=BNF.canPrecede)
     print "\tCan precede ", succ
 
@@ -68,6 +139,7 @@ def doProduction(lhs):
 	print "\toption: "+`option.value()`
 	for part in option:
 	    if part not in already and part not in agenda: agenda.append(part)
+	    y = `part`
 	conditions = g.each(subj=branch, pred=BNF.condition)
 	if conditions == []:
 	    print recordError(" NO SELECTOR for %s option %s ie %s" %(`lhs`, `option`, `option.value()` ))
@@ -82,6 +154,7 @@ def doProduction(lhs):
 				str1, branchDict[str1].value()))
 	    branchDict[str1.__str__()] = option
 #	    break
+
     for str1 in branchDict:
 	for str2 in branchDict:
 	    
@@ -151,7 +224,15 @@ class PredictiveParser:
 	    if word == "keywords" : parser.keywords = []	# Special
 	    return "@" + word, i  # implicit keyword
 	return "a", i    # qname, langcode, or barename
-    
+
+    def around(parser, str, this):
+	"The line around the given point"
+	sol = str.rfind("\n", this)
+	if sol <= 0: sol = 0
+	eol = str.find("\n", this)
+	if eol <= 0: eol = len(str)
+	return "On line %i at $ in: %s$%s" %(parser.lineNumber, str[sol:this], str[this:eol])
+	
     def parse(parser, str):
 	tok, this = parser.token(str, 0)
 	return parser.parseProduction(parser.top, str, tok, this)
@@ -163,8 +244,8 @@ class PredictiveParser:
 	lookupTable = parser.branchTable[lhs]
 	rhs = lookupTable.get(tok, None)  # Predict branch from token
 	if rhs == None: raise SyntaxError(
-		"Found %s when expecting some form of %s,\n\tsuch as %s"
-		    %(tok, lhs, lookupTable.keys()))
+		"Found %s when expecting some form of %s,\n\tsuch as %s\n\t%s"
+		    %(tok, lhs, lookupTable.keys(), parser.around(str, this)))
 	print "%i  %s means expand %s as %s" %(parser.lineNumber,tok, lhs, rhs.value())
 	for term in rhs:
 	    if isinstance(term, Literal):
@@ -173,7 +254,8 @@ class PredictiveParser:
 		if str[this:next] == lit: pass
 		elif "@"+str[this:next-1] == lit: next = next-1
 		else: raise SyntaxError(
-		    "Found %s where %s expected" % (str[this:next], lit))
+		    "Found %s where %s expected\n\t %s" %
+			(str[this:next], lit, parser.around(str, this)))
 	    else:
 		rexp = tokenRegexps.get(term, None)
 		if rexp == None: # Not token
@@ -181,7 +263,8 @@ class PredictiveParser:
 		    continue
 		m = rexp.match(str, this)
 		if m == None:
-		    raise SyntaxError("Token:  %s should match %s" % (val, rexp.pattern))
+		    raise SyntaxError("Token: should match %s\n\t" % 
+				(rexp.pattern, parser.around(str, this)))
 		next = m.end()
 	    tok, this = parser.token(str, next)  # Next token
 	return tok, this
@@ -201,6 +284,9 @@ document = g.newSymbol(argv[2])
 BNF = Namespace("http://www.w3.org/2000/10/swap/grammar/bnf#")
 #RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 
+already = []
+agenda = []
+errors = []
 doProduction(document)
 while agenda:
     x = agenda[0]
@@ -220,6 +306,14 @@ print "Literal terminals:", literalTerminals.keys()
 print "Token regular expressions:"
 for r in tokenRegexps: print "\t%s matches %s" %(r, tokenRegexps[r].pattern) 
 
+yacc=open(argv[1]+"-yacc.y", "w")
+yaccConvert(yacc, document, tokenRegexps)
+#while agenda:
+#    x = agenda[0]
+#    agenda = agenda[1:]
+#    already.append(x)
+#    yaccProduction(yacc, x, tokenRegexps)
+yacc.close()
 
 if len(argv) <= 3: exit(0)
 parseFile = argv[3]
