26895ada24b79087031a5c56ede3edd820e8667f
syosi <syosi>
2006-06-26 16:49:10 +0000

    preliminary set matching

--- vf2.py
+++ vf2.py
@@ -28,14 +28,13 @@ class Problem(object):
 
 class BindingTree(object):
     def __init__(self):
-        self._b = []
+        self._b = [[]]
 
     def ext_or(self, l):
         self._b.extend(l)
 
     def int_or(self, l):
         new_b = []
-        if not self._b: self._b = [[]]
         for choice in self._b:
             for choice2 in l:
                 if isinstance(choice2, (set, list)):
@@ -44,13 +43,10 @@ class BindingTree(object):
                     choice2 = [choice2]
                 new_b.append(choice + choice2)
         self._b = new_b
-        if self._b == [[]]: self._b = []
 
     def int_and(self, l):
-        if not self._b: self._b = [[]]
         for choice in self._b:
             choice.extend(list(l))
-        if self._b == [[]]: self._b = []
 
     def choices(self):
         for choice in self._b:
@@ -68,6 +64,11 @@ class BindingTree(object):
 
     def __len__(self):
         return len(self._b)
+
+    def isDefault(self):
+        return self._b == [[]]
+
+regular = []
 
 class OrderedNodeSet(set):
     def first(self):
@@ -122,30 +123,32 @@ on our regularly scheduled P(s)
     progress( 'starting match')
     progress('s.map=%s' % s.map)
     G2 = s.problem.G2
-    if not extras:
-        if set(s.map.values()) >= G2.allNodes():
-            yield s.map
-        elif set(s.map.values()) >= G2.nodes():
-            yield finish(s, s.map)
-            
-    if extras:
-        nodeList = extras.P()
-    else:
-        nodeList = P(s)
-    nodeList = [x for x in nodeList]
-    progress('nodeList=', nodeList)
-    for n,m, realExtras in nodeList:
-        progress('... trying n,m=%s,%s' % (n,m))
-        newExtras = BindingTree()
-        if realExtras:
+    for choice in extras:
+        if not choice:
+            if set(s.map.values()) >= G2.allNodes():
+                yield s.map
+            elif set(s.map.values()) >= G2.nodes():
+                yield finish(s, s.map)
+
+            nodeList = P(s)
+                
+        else:
+            n, m = choice[0]
+            nodeList = [(n, m, choice[1:])]
+
+        nodeList = [x for x in nodeList]
+        progress('nodeList=', nodeList)
+        for n,m, realExtras in nodeList:
+            progress('... trying n,m=%s,%s' % (n,m))
+            newExtras = BindingTree()
             newExtras.int_and(realExtras)
-        if F(s,n,m, newExtras):
-            s2 = s.addNode(n,m)
-            for x in match(s2, newExtras): yield x
-            s2.undo()
+            if F(s,n,m, newExtras):
+                s2 = s.addNode(n,m)
+                for x in match(s2, newExtras): yield x
+                s2.undo()
 
 def finish(s, mapping):
-    raise RuntimeError(s.problem.G1.allNodes() - set(mapping))
+    raise RuntimeError(s.problem.G2.allNodes() - set(mapping.values()), mapping)
         
 def P(s):
     """
@@ -160,18 +163,30 @@ def P(s):
     if t1_out_size and t2_out_size:
         progress(', case 1')
         m = s.t2_out.first()
-        for n in s.t1_out:
-            yield n, m, False
+        if representsSelf(m):
+            if m in s.t1_out:
+                yield m, m, regular
+        else:
+            for n in s.t1_out:
+                yield n, m, regular
     elif not t1_out_size and not t2_out_size and t1_in_size and t2_in_size:
         progress(', case 2')
         m = s.t2_in.first()
-        for n in s.t1_in:
-            yield n, m, False
+        if representsSelf(m):
+            if m in s.t1_in:
+                yield m, m, regular
+        else:
+            for n in s.t1_in:
+                yield n, m, regular
     elif not t1_out_size and not t2_out_size and not t1_in_size and not t2_in_size:
         progress(', case 3')
         m = s.G2_not_taken.first()
-        for n in s.G1_not_taken:
-            yield n, m, False
+        if representsSelf(m):
+            if m in s.G1_not_taken:
+                yield m, m, regular
+        else:
+            for n in s.G1_not_taken:
+                yield n, m, regular
     
 
 def F(s, n, m, extras):
@@ -245,8 +260,8 @@ def F(s, n, m, extras):
             if not e or not easyMatches(s, preds, e, newBindings):
                 progress(' -- failed because of edge')
                 return False
-            if newBindings:
-                extras.int_or(newBindings)
+##            if newBindings:
+##                extras.int_or(newBindings)
         else:
             if obj in s.t2_in:
                 termin2 += 1
@@ -263,8 +278,8 @@ def F(s, n, m, extras):
             if not e or not easyMatches(s, preds, e, newBindings):
                 progress(' -- failed because of edge')
                 return False
-            if newBindings:
-                extras.int_or(newBindings)
+##            if newBindings:
+##                extras.int_or(newBindings)
         else:
             if subj in s.t2_in:
                 termin2 += 1
@@ -313,13 +328,108 @@ def easyMatches(s, n1, n2, newBindings=BindingTree(), boring=True):
         n4 = n2.first
         if easyMatches(s, n3, n4, newBindings, False):
             return easyMatches(s, n1.rest, n2.rest, newBindings)
-    if isSet(n1) and isSet(n2):
-        if len(n1) != len(n2):
+    if isFormula(n1) and isFormula(n2):
+        matches = match(n1, n2)
+        if not matches:
             return False
+        newBindings.int_or([x.items() for x in matches])
+        return True
+    if isSet(n1) and isSet(n2):
+###  set matching
+###  This is annoying. We mostly will try to do anything to
+###  avoid this
+###
+        progress('Starting set matching on %s=?%s' % (n1, n2))
+    ## first test length
+        if len(n1) != len(n2):
+            progress('Different sizes!')
+            return False
+    ## easy cases are easy
         if len(n1) == 1:
+            progress('Size 1 --- easy')
             return easyMatches(s, iter(n1).next(), iter(n2).next(), newBindings, False)
+    ## knock out everything we know
+        for x1 in n1:
+            if x1 in s.map and s.map[x1] not in n2:
+                progress('map failed --- s[%s] == %s, couldn\'t find it' % (x1, s.map[x1]))
+                return False
+        n1 = set([x for x in n1 if x not in s.map])
+        n2 = set([x for x in n2 if x not in s.reverseMap])
+    ## redundant, hopefully
+        if len(n1) != len(n2):
+            progress('How did I get here?')
+            return False
+
+    ## sort into classes
+        tests = isLiteral, isSymbol, isExistential, isUniversal, isList, isFormula, isSet
+        g1 = tuple([set() for x in tests])
+        g2 = tuple([set() for x in tests])
+        for g, n3 in (g1, n1), (g2, n2):
+            for x in n3:
+                for i in range(len(tests)):
+                    test = tests[i]
+                    if test(x):
+                        g[i].add(x)
+                        break
+                else:
+                    raise TypeError(x)
+
+        progress('Sort got us %s,%s' % (g1,g2))
+    ## make sure every class works
+        if [len(x) for x in g1] != [len(x) for x in g2]:
+            progress('One class had a length mismatch!')
+            return False
+    ## literals and symbols must match themselves
+        for s1, s2 in (g1[0], g2[0]), (g1[1], g2[1]):
+            for x in s1:
+                if x not in s2:
+                    progress("Failed to find `%s' in s2=%s" % (x, s2))
+                    return False
+                newBindings.int_and([(x,x)])
+
+    ## rest are harder to do
+    ## do we ever want to enumerate this?
+        for s1, s2 in zip(g1[2:], g2[2:]):
+            b = []
+            s1 = list(s1)
+            for k in orderings(list(s2)):
+                b.append(zip(s1, k))
+            newBindings.int_or(b)
+
+        progress('After that, newBindings=%s' % newBindings)
         return True
     return False
+
+def orderings(l):
+    if not l:
+        yield l
+    else:
+        for i in range(len(l)):
+            l2 = l[:i] + l[i+1:]
+            x = [l[i]]
+            for o2 in orderings(l2):
+                yield x + o2    
+
+def memoizeEasyMatches(f):
+    saved = {}
+    def f2(s, n1, n2, *args, **keywords):
+        if isinstance(n1, set):
+            n11 = frozenset(n1)
+        else:
+            n11 = n1
+        if isinstance(n2, set):
+            n22 = frozenset(n2)
+        else:
+            n22 = n2
+        if (n11, n22) not in saved:
+            saved[(n11, n22)] = f(s, n1, n2, *args, **keywords)
+        return saved[(n11, n22)]
+    return f2
+
+## easyMatches = memoizeEasyMatches(easyMatches)
+
+def representsSelf(x):
+    return isLiteral(x) or isSymbol(x)
 
 def isLiteral(x):
     return isinstance(x, (str, unicode)) and x[0:1] == '"'
@@ -327,7 +437,11 @@ def isSymbol(x):
     return isinstance(x, (str, unicode)) and x[0:1] == '<'
 def isExistential(x):
     return isinstance(x, (str, unicode)) and x[0:1] == '_'
+def isUniversal(x):
+    return False
 def isList(x):
+    return False
+def isFormula(x):
     return False
 def isSet(x):
     return isinstance(x, (set, frozenset))
@@ -425,20 +539,34 @@ class Graph(object):
 	    if verbose: stderr.write( "Triple: %s  %s  %s.\n" % (triple[0], triple[1], triple[2]))
 	    yield triple
 
+def matchFormula(f1, f2):
+    m = {}
+    for n1 in f1.nodes():
+        if n1 in f2.nodes() and \
+           representsSelf(n1):
+            m[n1]=n1
+        elif representsSelf(n1):
+            return []
+    pr = Problem(G1=f1,G2=f2)
+    s = State(pr,m)
+    matches = [m for m in match(s)]
+    return matches
+            
+
 ### testing stuff
 def main():
     import sys
     g1 = Graph(sys.argv[1])
     g2 = Graph(sys.argv[2])
     m = {}
-    for n1 in g1.nodes():
-        if n1 in g2.nodes() and \
-           (isLiteral(n1) or isSymbol(n1)):
+    for n1 in g1.allNodes():
+        if n1 in g2.allNodes() and \
+           representsSelf(n1):
             m[n1]=n1
-        elif isLiteral(n1) or isSymbol(n1):
+        elif representsSelf(n1):
             return False
     pr = Problem(G1=g1,G2=g2)
-    s = State(pr,m)
+    s = State(pr)
     for m in match(s):
         print m
 
